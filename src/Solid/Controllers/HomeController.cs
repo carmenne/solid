505bb7c5227ad2dadf4774e65ad672ed3475afdb
carmenne <cpomana@NCBLAPTOP551.fas.local>
2016-10-07 11:41:58 +0200

    Add search controller

--- src/Solid/Controllers/HomeController.cs
+++ src/Solid/Controllers/HomeController.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Solid.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
